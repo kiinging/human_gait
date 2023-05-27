3cec89fe6250adb7e174282e57388543c84e7f93
wongkiinging <92498689+kiinging@users.noreply.github.com>
2023-05-27 22:31:55 +0800

    Delete reciever directory

--- reciever/boot.py
+++
@@ -1,8 +0,0 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
-import sys
-
-sys.path.append('/aioble')
